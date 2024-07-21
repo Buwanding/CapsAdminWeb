c789034885fec22c707a9a3cde66ebc2b3ceba5b
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2024-07-21 19:24:26 +0800

    Working displays for rider, customer and admin pages

--- caps-admin-web/src/components/screens/riders/RidersList.js
+++ caps-admin-web/src/components/screens/riders/RidersList.js
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import Sidenav from "../../parts/Sidenav";
 import Header from "../../parts/Header";
-import API_URL from "../../../api_url";
-
+import userService from "../../../services";
 
 const RidersList = () => {
   const [riders, setRiders] = useState([]);
@@ -11,8 +9,8 @@ const RidersList = () => {
   useEffect(() => {
     const fetchRiders = async () => {
       try {
-        const response = await axios.get(API_URL+ 'user/riders');
-        setRiders(response.data);
+        const data = await userService.fetchRiders();
+        setRiders(data);
       } catch (error) {
         console.error("There was an error fetching the riders!", error);
       }
@@ -24,9 +22,9 @@ const RidersList = () => {
   return (
     <div className="flex">
       <Sidenav />
-      <div className="flex-1">
+      <div className="flex flex-col w-full">
         <Header />
-        <main className="p-4">
+        <main className="flex-grow p-4 bg-gray-100">
           <div className="bg-white shadow-md rounded-lg p-6">
             <div className="flex justify-between items-center mb-4">
               <h1 className="text-2xl font-bold">RIDERS LIST</h1>
@@ -45,6 +43,7 @@ const RidersList = () => {
                   <th className="px-4 py-2">Name</th>
                   <th className="px-4 py-2">Phone Number</th>
                   <th className="px-4 py-2">Status</th>
+                  <th className="px-4 py-2">Verification</th>
                 </tr>
               </thead>
               <tbody>
@@ -62,6 +61,19 @@ const RidersList = () => {
                         <span className="inline-flex items-center">
                           <span className="w-3 h-3 bg-red-500 rounded-full mr-2"></span>
                           <span>{rider.status}</span>
+                        </span>
+                      )}
+                    </td>
+                    <td className="px-4 py-2">
+                      {rider.rider?.verification_status === "Verified" ? (
+                        <span className="inline-flex items-center">
+                          <span className="w-3 h-3 bg-green-500 rounded-full mr-2"></span>
+                          <span>Verified</span>
+                        </span>
+                      ) : (
+                        <span className="inline-flex items-center">
+                          <span className="w-3 h-3 bg-yellow-500 rounded-full mr-2"></span>
+                          <span>Pending</span>
                         </span>
                       )}
                     </td>

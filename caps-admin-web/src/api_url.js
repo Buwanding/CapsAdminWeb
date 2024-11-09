47c2dcf56edf7a5a24ac5f05b63a754cf883a06e
Raphael Alingig <124657190+raphaelalingig@users.noreply.github.com>
2024-11-09 18:53:23 +0800

    Update api_url.js

--- caps-admin-web/src/api_url.js
+++ caps-admin-web/src/api_url.js
@@ -1,4 +1,4 @@
-const img_url = "http://192.168.1.17:8000"; //<----Change IP address here only
+const img_url = "https://backendpickmeuptrial-production.up.railway.app"; //<----Change IP address here only
 const API_URL = img_url + "/api/user/";
 
 export { API_URL, img_url };
